c5dc9de878db2d016291378252c7f3d53ccc34c1
Sumit Singh <sumitrajput7079@gmail.com>
2025-12-18 16:51:19 +0530

    -Create fetch,update,delete api and an alter migration table to show is_deleted status.

--- Manage_addresses/app/Http/Controllers/AddressController.php
+++ Manage_addresses/app/Http/Controllers/AddressController.php
@@ -54,4 +54,208 @@ class AddressController extends Controller
             ]);
         }
     }
+    /**Function to Get all addresses */
+    public function getAddresses()
+    {
+        try {
+            $addresses = Address::where([["is_deleted", false]])->get();
+            return response()->json([
+                "status" => "success",
+                "status_code" => 200,
+                "message" => "Addresses retrieved successfully",
+                "data" => $addresses
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                "status" => "failure",
+                "status_code" => 500,
+                "message" => "Server Error",
+                "errors" => $e->getMessage()
+            ]);
+        }
+    }
+    /**Function to update an address */
+    public function updateAddressDetails(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                "id" => "required",
+            ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    "status" => "failure",
+                    "status_code" => 400,
+                    "message" => "Validation Error",
+                    "errors" => $validator->errors()
+                ]);
+            }
+            $address = Address::where([["id", $request->id]])->first();
+            if (!$address) {
+                return response()->json([
+                    "status" => "failure",
+                    "status_code" => 404,
+                    "message" => "Address not found",
+                    "data" => []
+                ]);
+            }
+            if ($request->has('name')) {
+                $address->name = $request->name;
+            }
+            if ($request->has('mobile')) {
+                $address->mobile = $request->mobile;
+            }
+            if ($request->has('address')) {
+                $address->address = $request->address;
+            }
+            if ($request->has('city')) {
+                $address->city = $request->city;
+            }
+            if ($request->has('state')) {
+                $address->state = $request->state;
+            }
+            if ($request->has('pincode')) {
+                $address->pincode = $request->pincode;
+            }
+            if ($request->has('type')) {
+                $address->type = $request->type;
+            }
+            $address->save();
+
+            return response()->json([
+                "status" => "success",
+                "status_code" => 200,
+                "message" => "Address details updated successfully",
+                "data" => $address
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                "status" => "failure",
+                "status_code" => 500,
+                "message" => "Server Error",
+                "errors" => $e->getMessage()
+            ]);
+        }
+    }
+    /**Function to delete an address */
+    public function deleteAddressById(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                "id" => "required",
+            ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    "status" => "failure",
+                    "status_code" => 400,
+                    "message" => "Validation Error",
+                    "errors" => $validator->errors()
+                ]);
+            }
+            $address = Address::where([["id", $request->id]])->first();
+            if (!$address) {
+                return response()->json([
+                    "status" => "failure",
+                    "status_code" => 404,
+                    "message" => "Address not found",
+                    "data" => []
+                ]);
+            }
+            $address->delete();
+            return response()->json([
+                "status" => "success",
+                "status_code" => 200,
+                "message" => "Address deleted successfully",
+                "data" => []
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                "status" => "failure",
+                "status_code" => 500,
+                "message" => "Server Error",
+                "errors" => $e->getMessage()
+            ]);
+        }
+    }
+    /**Function to markIsDeleted true */
+    public function markIsdeletedTrue(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                "id" => "required",
+            ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    "status" => "failure",
+                    "status_code" => 400,
+                    "message" => "Validation Error",
+                    "errors" => $validator->errors()
+                ]);
+            }
+            $address = Address::where([["id", $request->id]])->first();
+            if (!$address) {
+                return response()->json([
+                    "status" => "failure",
+                    "status_code" => 404,
+                    "message" => "Address not found",
+                    "data" => []
+                ]);
+            }
+            $address->is_deleted = true;
+            $address->save();
+            return response()->json([
+                "status" => "success",
+                "status_code" => 200,
+                "message" => "Address marked as deleted successfully",
+                "data" => $address
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                "status" => "failure",
+                "status_code" => 500,
+                "message" => "Server Error",
+                "errors" => $e->getMessage()
+            ]);
+        }
+    }
+    /**Function to markIsDeleted false */
+    public function markIsdeletedFalse(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                "id" => "required",
+            ]);
+            if ($validator->fails()) {
+                return response()->json([
+                    "status" => "failure",
+                    "status_code" => 400,
+                    "message" => "Validation Error",
+                    "errors" => $validator->errors()
+                ]);
+            }
+            $address = Address::where([["id", $request->id]])->first();
+            if (!$address) {
+                return response()->json([
+                    "status" => "failure",
+                    "status_code" => 404,
+                    "message" => "Address not found",
+                    "data" => []
+                ]);
+            }
+            $address->is_deleted = false;
+            $address->save();
+            return response()->json([
+                "status" => "success",
+                "status_code" => 200,
+                "message" => "Address marked as not deleted successfully",
+                "data" => $address
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                "status" => "failure",
+                "status_code" => 500,
+                "message" => "Server Error",
+                "errors" => $e->getMessage()
+            ]);
+        }
+    }
 }
--- Manage_addresses/routes/api.php
+++ Manage_addresses/routes/api.php
@@ -4,5 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AddressController;
 
+
 /**Route to add a new address */
 Route::post("/address/new", [AddressController::class, 'newAddress']);
+/**Route to get all addresses */
+Route::get("/addresses", [AddressController::class, 'getAddresses']);
+/**Route to update an address */
+Route::get("/address/update/details", [AddressController::class, 'updateAddressDetails']);
+/**Route to delete an address */
+Route::get("/address/delete", [AddressController::class, 'deleteAddressById']);
+/**Route to mark student as is deleted true */
+Route::delete("/address/delete", [AddressController::class, 'markIsdeletedTrue']);
+/**Route to mark student as is deleted false */
+Route::put("/address/delete", [AddressController::class, 'markIsdeletedFalse']);
